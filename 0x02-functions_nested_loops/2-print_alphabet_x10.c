835ff05b2a4ebbc388f31814ca6937745535d3af
ZHAYRI123 <merykozan@gmail.com>
2023-03-22 16:35:11 -0700

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *print_alphabet - print alphabet
+ *print_alphabet_x10  - print alphabet
  *loop to print letter 10
  * Return : 0
  */
@@ -14,6 +14,8 @@ for (i = 1 ; i <= 10 ; i++)
 	{
 	for (c = 'a' ; c <= 'z' ; c++)
 		_putchar(c);
+	
 	}
+	
 	_putchar('\n');
 }
