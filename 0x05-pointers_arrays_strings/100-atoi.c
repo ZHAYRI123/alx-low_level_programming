533b139018cdb8db21663f115925f60fe001c6b2
ZHAYRI123 <merykozan@gmail.com>
2023-03-27 17:13:07 -0700

    update

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,14 +16,16 @@ int _atoi(char *s)
 	{
 		if (s[c] == 45)
 			min *= -1;
-		while (s[c] >= 48 && s[c] <= 54)
+		while (s[c] >= 48 && s[c] <= 57)
 		{
 			isi = 1;
 			ni = (ni * 10) + (s[c] - '0');
 			c++;
 		}
 		if (isi == 1)
+		{
 			break;
+		}
 		c++;
 	}
 	ni *= min;
