f042e83221aa101a5082781c8f5e0b672a16013e
ZHAYRI123 <merykozan@gmail.com>
2023-03-27 17:05:43 -0700

    update

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 size_t _strlen(const char *s);
 int _putchar(char);
+int _atoi(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
