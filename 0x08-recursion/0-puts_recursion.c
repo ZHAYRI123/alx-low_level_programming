aa09c8026bc4e8284b12cdcef5cfe4fcee78b3a6
ZHAYRI123 <merykozan@gmail.com>
2023-04-19 08:42:11 -0700

    update

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _puts_recursion - function
+ * @s: input
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	printf("%c", *s);
+	_puts_recursion(++s);
+}
