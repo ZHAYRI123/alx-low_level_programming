04c105173784f22c831fd27126e261a901efd447
ZHAYRI123 <merykozan@gmail.com>
2023-04-21 06:20:15 -0700

    update

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -19,8 +19,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (0);
 	for (i = 0; i < size; i++)
 	{
-		cmp(array[i]);
-		return (i);
+		if (cmp(array[i]))
+			return (i);
 	}
 	return (-1);
 }
