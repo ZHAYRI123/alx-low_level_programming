b5c0de490b15501c9c242581c3b77822cf3ab637
ZHAYRI123 <merykozan@gmail.com>
2023-04-23 08:59:45 -0700

    update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - function
+ * @argc: input
+ * @argv: array
+ * *Return: ctring
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
