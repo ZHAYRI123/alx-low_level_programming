de5ec9e01da3cbe56baa8f78a473206dffd5fc71
ZHAYRI123 <merykozan@gmail.com>
2023-05-05 07:58:13 -0700

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,7 +13,7 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != 0 && b[i] != 1)
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		value = 2 * (value + b[i]);
 	}
