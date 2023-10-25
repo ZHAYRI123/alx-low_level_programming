403dd1f0649e9e31e1ca2def78ca8a31ede60f3a
ZHAYRI123 <merykozan@gmail.com>
2023-10-25 13:00:24 -0700

    update

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,7 +2,7 @@
 /**
  * dlistint_len - fct returns the number of elements in a linked list
  * @h: input
- * return: number of elements
+ * Return: number of elements
  */
 size_t dlistint_len(const dlistint_t *h)
 {
